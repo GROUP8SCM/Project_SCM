b70e01be9253db723556fcb87965227adb07ec9d
aisyahmatsaman <37771411+aisyahmatsaman@users.noreply.github.com>
2018-05-21 22:22:32 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,21 +1,6 @@
 #include<stdio.h>
-
-void getSum(int, int);
 void main()
 {
-    int num1, num2;
-    printf("Enter number 1:");
-    //scanf("%d",&num1);
-    printf("Enter number 2:");
-    //scanf("%d",&num2);
-    getSum(num1,num2);
-    
-    return 0;
+    printf("Hello world");
 }
 
-/*void getSum(int num1, int num2)
-{
-    int sum;
-    sum = num1+num2;
-    printf("the sum is %d",sum);
-}*/
