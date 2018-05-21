a1c60191844f121aa6e694ddee3cc65a396b2d8b
aisyahmatsaman <37771411+aisyahmatsaman@users.noreply.github.com>
2018-05-21 21:59:03 +0800

    Update Summation.c

--- Summation.c
+++ Summation.c
@@ -5,17 +5,17 @@ void main()
 {
     int num1, num2;
     printf("Enter number 1:");
-    scanf("%d",&num1);
+    //scanf("%d",&num1);
     printf("Enter number 2:");
-    scanf("%d",&num2);
+    //scanf("%d",&num2);
     getSum(num1,num2);
     
     return 0;
 }
 
-void getSum(int num1, int num2)
+/*void getSum(int num1, int num2)
 {
     int sum;
     sum = num1+num2;
     printf("the sum is %d",sum);
-}
+}*/
